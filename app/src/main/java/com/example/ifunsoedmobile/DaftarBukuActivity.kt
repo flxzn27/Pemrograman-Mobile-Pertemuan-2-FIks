6502afc90cd32aa0ed5f1c9f2e3083408a40100f
flxzn27 <flxzn406@gmail.com>
2025-09-26 14:18:31 +0700

    Tugas 3 Pertemuan 4

---
+++ app/src/main/java/com/example/ifunsoedmobile/DaftarBukuActivity.kt
@@ -0,0 +1,32 @@
+package com.example.ifunsoedmobile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ifunsoedmobile.databinding.ActivityDaftarBukuBinding
+import com.example.ifunsoedmobile.ui.adapter.BookAdapter
+import com.example.ifunsoedmobile.viewmodel.MainViewModel
+import androidx.activity.viewModels
+
+class DaftarBukuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDaftarBukuBinding
+    private val viewModel: MainViewModel by viewModels()
+    private val adapter = BookAdapter(emptyList())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDaftarBukuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+        viewModel.books.observe(this) { books ->
+            adapter.setData(books)
+        }
+
+        viewModel.fetchBooks(query = "kotlin programming")
+    }
+}
